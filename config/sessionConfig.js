beeb078f752a874bbdcfd72b1bba1d1d6a92b9e9
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-08-12 13:01:27 +0530

    Update sessionConfig.js

--- config/sessionConfig.js
+++ config/sessionConfig.js
@@ -1,15 +1,19 @@
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
 dotenv.config();
 
 export default session({
-  secret: process.env.SESSION_SECRET || "supersecret",
+  secret:process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,              // your MongoDB connection string
+    collectionName: "sessions",                    // optional, default is 'sessions'
+  }),
   cookie: {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     httpOnly: true,
-    secure: true,
-    sameSite: "none"
+    secure:process.env.NODE_ENV === "production", // true in production with HTTPS
   }
 });

aed4f59163d48973f8396966099ed1679fce8d51
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-08-12 11:16:38 +0530

    Update sessionConfig.js

--- config/sessionConfig.js
+++ config/sessionConfig.js
@@ -5,10 +5,11 @@ dotenv.config();
 export default session({
   secret: process.env.SESSION_SECRET || "supersecret",
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     httpOnly: true,
-    secure: false // true in production with HTTPS
+    secure: true,
+    sameSite: "none"
   }
 });
